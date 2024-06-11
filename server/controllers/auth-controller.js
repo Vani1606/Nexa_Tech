ad89d4a552de0ea3e89f37cd3fc92bfc0302e113
DV <divyanshuv07@gmail.com>
2024-06-12 00:30:27 +0530

    MongoDB Database connected

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -20,7 +20,8 @@ const home = async (req, res) => {
   // *-------------------
   const register = async (req, res) => {
     try {
-      res.status(200).send({ message: "registration page" });
+      const data = req.body;
+      res.status(200).json({ msg: data });
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const router = require("./routes/auth-route");
+const connectDb = require("./utils/db");
 
 // Mount the Router: To use the router in your main Express app, you can "mount" it at a specific URL prefix
 app.use("/api/auth", router);
 
 const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`server is running at port: ${PORT}`);
+connectDb.then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`);
+  });
 });
