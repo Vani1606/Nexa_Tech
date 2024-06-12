8592909de24d7b7e12d0e2e4cb0b711a93b201e6
DV <divyanshuv07@gmail.com>
2024-06-12 11:20:56 +0530

    Added user model

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,30 +1,45 @@
 // *----------------------
 //* Controllers
 // *----------------------
-
 //? In an Express.js application, a "controller" refers to a part of your code that is responsible for handling the application's logic. Controllers are typically used to process incoming requests, interact with models (data sources), and send responses back to clients. They help organize your application by separating concerns and following the MVC (Model-View-Controller) design pattern.
+
+const User = require("../models/user-model");
 
 // *-------------------
 // Home Logic
 // *-------------------
 const home = async (req, res) => {
-    try {
-      res.status(200).json({ msg: "Welcome to our home page" });
-    } catch (error) {
-      console.log(error);
+  try {
+    res.status(200).json({ msg: "Welcome to our home page" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// *-------------------
+// Registration Logic
+// *-------------------
+const register = async (req, res) => {
+  try {
+
+    const { username, email, phone, password } = req.body;
+
+    const userExist = await User.findOne({ email: email });
+
+    if (userExist) {
+      return res.status(400).json({ msg: "email already exists" });
     }
-  };
-  
-  // *-------------------
-  // Registration Logic
-  // *-------------------
-  const register = async (req, res) => {
-    try {
-      const data = req.body;
-      res.status(200).json({ msg: data });
-    } catch (error) {
-      res.status(500).json({ message: "Internal server error" });
-    }
-  };
-  
-  module.exports = { home, register };
+
+    const userCreated = await User.create({ username, email, phone, password });
+
+    res.status(201).json({
+      msg: "Registration Successful",
+      token: await userCreated.generateToken(),
+      userId: userCreated._id.toString(),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = { home, register };
--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const router = require("./routes/auth-route");
+const authRoute=require("./router/auth-router");
 const connectDb = require("./utils/db");
 
 // Mount the Router: To use the router in your main Express app, you can "mount" it at a specific URL prefix
-app.use("/api/auth", router);
+app.use("/api/auth", authRoute);
 
 const PORT = 5000;
-connectDb.then(() => {
+connectDb().then(() => {
   app.listen(PORT, () => {
     console.log(`server is running at port: ${PORT}`);
   });
