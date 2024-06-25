3353a08ad70438ced16771a12789cbe0465bedff
Vani Tripathi <vanitripathi1103@gmail.com>
2024-06-25 18:26:32 +0530

    auth middleware added

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user-model");
+const Contact = require("../models/contact-model");
 
 // *-------------------------------
 //* getAllUsers Logic 📝
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -25,7 +25,7 @@ const userSchema=new mongoose.Schema({
 
     isAdmin:{
         type:Boolean,
-        required:false,
+        default:false,
     },
 });
 
