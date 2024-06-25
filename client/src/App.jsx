b8a55d3e399b92d23ad2dc59b691b7bb08c7efdd
Vani Tripathi <vanitripathi1103@gmail.com>
2024-06-25 19:05:18 +0530

    admin layout added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import { Login } from "./pages/Login";
 import { Logout } from "./pages/Logout";
 import { Navbar } from "./components/Navbar";
 import { Error } from "./pages/Error";
+import { AdminLayout } from "./components/layouts/Admin-Layout";
+import { AdminUsers } from "./pages/Admin-Users"; // Import AdminUsers
+import { AdminContacts } from "./pages/Admin-Contacts"; // Import AdminContacts
 
 const App = () => {
   return (
@@ -22,6 +25,10 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="*" element={<Error />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="users" element={<AdminUsers />} />
+          <Route path="contacts" element={<AdminContacts />} />
+        </Route>
       </Routes>
     </Router>
   );
