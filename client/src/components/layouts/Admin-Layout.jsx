09c903992d9cceda8eab7eec9bd37f39d9541c05
DV <divyanshuv07@gmail.com>
2024-06-26 15:26:57 +0530

    Created Admin-Users page

--- client/src/components/layouts/Admin-Layout.jsx
+++ client/src/components/layouts/Admin-Layout.jsx
@@ -23,7 +23,7 @@ export const AdminLayout = () => {
             <ul>
               <li>
                 <NavLink to="/admin/users">
-                  <FaUser /> users
+                  <FaUser /> Users
                 </NavLink>
               </li>
               <li>
--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -33,5 +33,18 @@ const getAllContacts = async (req, res) => {
     }
 };
 
+// *-------------------------------
+//* user delete Logic 📝
+// *-------------------------------
 
-module.exports = { getAllUsers, getAllContacts };
+const deleteUserById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await User.deleteOne({ _id: id });
+        return res.status(200).json({ message: "User Deleted Successfully" });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { getAllUsers, getAllContacts, deleteUserById };
